1c1d7443fcf4a96f240c6e47aa7715de8a8afb1c
hank <pang5792@gmail.com>
2019-03-05 18:38:11 +0800

    增加 server_status 與 device_status 頁面[feature]

--- frontend.py
+++ frontend.py
@@ -22,24 +22,24 @@ frontend = Blueprint('frontend', __name__)
 # navbar has an usual amount of Link-Elements, more commonly you will have a
 # lot more View instances.
 nav.register_element('frontend_top', Navbar(
-    View('Flask-Bootstrap', '.index'),
-    View('Home', '.index'),
+    # View('Flask-Bootstrap', '.index'),
+    # View('Home', '.index'),
     View('Home Neo Make', '.indexV2'),
     View('Forms Example', '.example_form'),
     View('Debug-Info', 'debug.debug_root'),
-    Subgroup(
-        'Docs',
-        Link('Flask-Bootstrap', 'http://pythonhosted.org/Flask-Bootstrap'),
-        Link('Flask-AppConfig', 'https://github.com/mbr/flask-appconfig'),
-        Link('Flask-Debug', 'https://github.com/mbr/flask-debug'),
-        Separator(),
-        Text('Bootstrap'),
-        Link('Getting started', 'http://getbootstrap.com/getting-started/'),
-        Link('CSS', 'http://getbootstrap.com/css/'),
-        Link('Components', 'http://getbootstrap.com/components/'),
-        Link('Javascript', 'http://getbootstrap.com/javascript/'),
-        Link('Customize', 'http://getbootstrap.com/customize/'), ),
-    Text('Using Flask-Bootstrap {}'.format(FLASK_BOOTSTRAP_VERSION)), ))
+    # Subgroup(
+    #     'Docs',
+    #     Link('Flask-Bootstrap', 'http://pythonhosted.org/Flask-Bootstrap'),
+    #     Link('Flask-AppConfig', 'https://github.com/mbr/flask-appconfig'),
+    #     Link('Flask-Debug', 'https://github.com/mbr/flask-debug'),
+    #     Separator(),
+    #     Text('Bootstrap'),
+        # Link('Getting started', 'http://getbootstrap.com/getting-started/'),
+    #     Link('CSS', 'http://getbootstrap.com/css/'),
+    #     Link('Components', 'http://getbootstrap.com/components/'),
+    #     Link('Javascript', 'http://getbootstrap.com/javascript/'),
+    #     Link('Customize', 'http://getbootstrap.com/customize/'), ),
+    Text('Using Flask-Bootstrap {}'.format(FLASK_BOOTSTRAP_VERSION)),))
 
 
 # Our index-page just shows a quick explanation. Check out the template
@@ -72,3 +72,15 @@ def example_form():
 @frontend.route('/indexV2/')
 def indexV2():
     return render_template('indexV2.html', hosts=ut.Utils.get_json_file('./FDI_MOBILE_CONSOLE/utils/index.json'))
+
+@frontend.route('/server_status/<serverName>/')
+def server_status(serverName):
+    print('serverName')
+    print(serverName)
+    return render_template('server_status.html', serverName=serverName, apiLogs=ut.Utils.get_json_file('./FDI_MOBILE_CONSOLE/utils/api_log.json'))
+
+@frontend.route('/device_status/<serianlNo>')
+def device_status(serianlNo):
+    print('serianlNo')
+    print(serianlNo)
+    return render_template('device_status.html', serianlNo=serianlNo, deviceLogs=ut.Utils.get_json_file('./FDI_MOBILE_CONSOLE/utils/device_log.json'))
